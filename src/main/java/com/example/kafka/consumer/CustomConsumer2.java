841114af1d474c9ac29500e01c1090494ec7f25f
bailuoxi66 <bailuoxihaha@163.com>
2022-11-29 22:23:01 +0800

    Kafka 消费者分区策略为StickyAssignor

--- src/main/java/com/example/kafka/consumer/CustomConsumer2.java
+++ src/main/java/com/example/kafka/consumer/CustomConsumer2.java
@@ -22,10 +22,10 @@ public class CustomConsumer2 {
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "test2");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "test5");
 
         // 设置分区分配策略
-        properties.put(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, "org.apache.kafka.clients.consumer.RoundRobinAssignor");
+        properties.put(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, "org.apache.kafka.clients.consumer.StickyAssignor");
 
         // 1. 创建一个消费者 "", "hello"
         KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
