f4da6e3786e09bd6391e7f2e11a62898602ca072
bailuoxi66 <bailuoxihaha@163.com>
2022-11-29 22:27:04 +0800

    Kafka 消费者自动提交offset

---
+++ src/main/java/com/example/kafka/consumer/CustomConsumerAutoOffset.java
@@ -0,0 +1,53 @@
+package com.example.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class CustomConsumerAutoOffset {
+    public static void main(String[] args) {
+
+        // 0 配置
+        Properties properties = new Properties();
+        // 连接 bootstrap.servers.
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "hadoop102:9092,hadoop103:9092");
+
+        // 反序列化
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
+
+        // 自动提交
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, true);
+
+        // 提交时间间隔
+        properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 1000);
+
+        // 设置分区分配策略
+        properties.put(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, "org.apache.kafka.clients.consumer.StickyAssignor");
+
+        // 1. 创建一个消费者 "", "hello"
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
+
+        // 2. 订阅主题first
+        ArrayList<String> topics = new ArrayList<>();
+        topics.add("first");
+        kafkaConsumer.subscribe(topics);
+
+        // 3. 消费数据
+        while (true) {
+            ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofSeconds(1));
+
+            for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
+                System.out.println(consumerRecord);
+            }
+        }
+    }
+}
